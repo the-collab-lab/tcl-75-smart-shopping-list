caf21571c010229e333748e010055ad5f8d2d2e6
Nika Kolesnikova <firstnamenika@gmail.com>
2024-09-01 12:23:13 -0400

    remove redundant false return in shareList function

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -146,7 +146,7 @@ export async function shareList(listPath, currentUserId, recipientEmail) {
 	const recipientDoc = await getDoc(doc(usersCollectionRef, recipientEmail));
 	// If the recipient user doesn't exist, we can't share the list.
 	if (!recipientDoc.exists()) {
-		return false;
+		return;
 	}
 	// Add the list to the recipient user's sharedLists array.
 	const listDocumentRef = doc(db, listPath);
