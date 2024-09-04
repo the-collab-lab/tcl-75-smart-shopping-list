9e71e6edcdddfcef142d2fcee3695da1581dcc02
Nika Kolesnikova <firstnamenika@gmail.com>
2024-09-04 11:33:18 -0400

    added an else block to the return statement that checks whether data props is empty; if empty show a welcoming message

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,8 +1,10 @@
 import { ListItem } from '../components';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function List({ data }) {
 	const [searchItem, setSearchItem] = useState('');
+	const navigate = useNavigate();
 
 	const handleTextChange = (event) => {
 		setSearchItem(event.target.value);
@@ -14,33 +16,46 @@ export function List({ data }) {
 
 	return (
 		<>
-			<form onSubmit={(event) => event.preventDefault()}>
-				<label htmlFor="search-item">Search Item: </label>
+			{data.length === 0 ? (
+				<>
+					{/* TODO Add a list name to make it more clear which list we are on */}
+					<p>
+						Hey! Ready to add your first item? Click button below to start your
+						list!
+					</p>
+					<button onClick={() => navigate('/manage-list')}>Add item!</button>
+				</>
+			) : (
+				<>
+					<form onSubmit={(event) => event.preventDefault()}>
+						<label htmlFor="search-item">Search Item: </label>
 
-				<input
-					id="search-item"
-					type="search"
-					placeholder="Search Item..."
-					onChange={handleTextChange}
-					value={searchItem}
-				/>
+						<input
+							id="search-item"
+							type="search"
+							placeholder="Search Item..."
+							onChange={handleTextChange}
+							value={searchItem}
+						/>
 
-				{searchItem && (
-					<button
-						type="button"
-						onClick={() => {
-							setSearchItem('');
-						}}
-					>
-						X
-					</button>
-				)}
-			</form>
-			<ul>
-				{filteredItems.map((item) => {
-					return <ListItem key={item.id} name={item.name} />;
-				})}
-			</ul>
+						{searchItem && (
+							<button
+								type="button"
+								onClick={() => {
+									setSearchItem('');
+								}}
+							>
+								X
+							</button>
+						)}
+					</form>
+					<ul>
+						{filteredItems.map((item) => {
+							return <ListItem key={item.id} name={item.name} />;
+						})}
+					</ul>
+				</>
+			)}
 		</>
 	);
 }
