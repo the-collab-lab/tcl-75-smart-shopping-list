22b7f76640929e6445914d18fb05917df023c829
Nika Kolesnikova <firstnamenika@gmail.com>
2024-09-07 10:24:27 -0400

    update TextInputElement to handle onChange prop

--- src/components/TextInputElement.jsx
+++ src/components/TextInputElement.jsx
@@ -1,9 +1,15 @@
-const TextInputElement = ({ children, type, id, placeholder }) => {
+const TextInputElement = ({ children, type, id, placeholder, onChange }) => {
 	return (
 		<>
 			<label htmlFor={id}>{children}</label>
 			<br />
-			<input type={type} id={id} placeholder={placeholder} required />
+			<input
+				type={type}
+				id={id}
+				placeholder={placeholder}
+				onChange={onChange}
+				required
+			/>
 			<br />
 		</>
 	);
