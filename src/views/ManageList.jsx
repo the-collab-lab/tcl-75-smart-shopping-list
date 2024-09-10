741c6ca2b7541e71822fa83fd5b5013cf9f76b49
Warren Chan <warren.chan0113@gmail.com>
2024-09-10 11:27:52 -0400

    passed data prop to AddItems

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -1,10 +1,10 @@
 import { AddItems } from '../components/AddItems';
 import { ShareList } from '../components/ShareList';
 
-export function ManageList() {
+export function ManageList({ data }) {
 	return (
 		<div>
-			<AddItems />
+			<AddItems data={data} />
 			<ShareList />
 		</div>
 	);
