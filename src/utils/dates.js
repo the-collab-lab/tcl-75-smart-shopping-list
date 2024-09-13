08b9a4cf747535e10bc33e3f74accb9dc8f2dcf8
Nika Kolesnikova <firstnamenika@gmail.com>
2024-09-13 15:44:34 -0400

    Add `calculatePurchaseIntervals` function for purchase date calculations
    - Introduced `calculatePurchaseIntervals` function to calculate intervals between purchase dates.
    - Added logging to display the previous purchase date, next purchase date, and calculated intervals.
    - Updated code to handle cases where `dateLastPurchased` may be null or undefined.

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -50,9 +50,61 @@ function getDaysBetweenDates(earlierDate, laterDate) {
 	return Math.floor(
 		(laterDate.getTime() - earlierDate.getTime()) / ONE_DAY_IN_MILLISECONDS,
 	);
+}
+
+function calculatePurchaseIntervals(
+	currentDate,
+	dateNextPurchased,
+	dateLastPurchased,
+) {
+	const lastPurchaseDate = dateLastPurchased?.toDate(); // valid to be null
+
 	console.log(
-		`Calculated ${daysBetweenDates} day(s) between 
-		${earlierDate.toLocaleString()} and ${laterDate.toLocaleString()}`,
+		`Previously purchased on: ${lastPurchaseDate === undefined ? undefined : lastPurchaseDate.toLocaleString()}`,
 	);
-	return daysBetweenDates;
+	console.log(
+		`Previous next purchase date: ${dateNextPurchased.toDate().toLocaleString()}`,
+	);
+
+	// valid to be undefined
+	const lastEstimatedInterval =
+		lastPurchaseDate === undefined
+			? undefined
+			: getDaysBetweenDates(lastPurchaseDate, dateNextPurchased.toDate());
+
+	console.log(`Last estimated interval: ${lastEstimatedInterval}`);
+
+	// has to be a number
+	const daysSinceLastPurchase =
+		lastPurchaseDate === undefined
+			? 0
+			: getDaysBetweenDates(lastPurchaseDate, currentDate);
+
+	console.log(`Days since last purchase: ${daysSinceLastPurchase}`);
+
+	return { lastEstimatedInterval, daysSinceLastPurchase };
+}
+
+function getNextPurchaseEstimate(purchaseIntervals, totalPurchases) {
+	console.log(`Total purchases: ${totalPurchases}`);
+	const { lastEstimatedInterval, daysSinceLastPurchase } = purchaseIntervals;
+
+	try {
+		// calculate the next purchase estimate
+		const estimatedDaysUntilPurchase = calculateEstimate(
+			lastEstimatedInterval, // valid to be undefined
+			daysSinceLastPurchase, // has to be a number
+			totalPurchases,
+		);
+
+		console.log(
+			`Estimated days until next purchase: ${estimatedDaysUntilPurchase}`,
+		);
+
+		const nextPurchaseEstimate = getFutureDate(estimatedDaysUntilPurchase);
+
+		return nextPurchaseEstimate;
+	} catch (error) {
+		throw new Error(`Failed updaing date next purchased: ${error}`);
+	}
 }
