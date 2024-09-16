84845a46fc9945be7800d5ba7f4ac2f1ac612cd0
Nika Kolesnikova <firstnamenika@gmail.com>
2024-09-16 16:22:45 -0400

    fix: add fixes to ManageList component type

--- src/views/ManageList.tsx
+++ src/views/ManageList.tsx
@@ -1,9 +1,9 @@
+import { DocumentData } from 'firebase/firestore';
 import { AddItems } from '../components/AddItems';
 import { ShareList } from '../components/ShareList';
-import { Item } from '../types/types';
 
 type ManageListProps = {
-	items: Item[];
+	items: DocumentData[];
 };
 
 export function ManageList({ items }: ManageListProps) {
