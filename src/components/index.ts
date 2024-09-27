edf3f70fd1da4e0341f36139f9b3951862dfbc7f
Nika Kolesnikova <firstnamenika@gmail.com>
2024-09-27 10:45:37 -0400

    feat: add exports to index.ts to keep imports shorter

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,6 @@
-export * from './ListItem';
 export * from './SingleList';
+export * from './ShareList';
+export * from './ListItem';
+export * from './AddItems';
+export * from './RadioInputElement';
+export * from './TextInputElement';
