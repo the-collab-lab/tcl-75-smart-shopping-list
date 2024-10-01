440720eaca46e31c5a5a67f96312db59cd729572
Warren Chan <warren.chan0113@gmail.com>
2024-10-01 13:46:14 -0400

    remove contain if block to make UX better

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import './ListItem.css';
 import { updateItem, deleteItem } from '../api';
 import { calculateDateNextPurchased, ONE_DAY_IN_MILLISECONDS } from '../utils';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const currentDate = new Date();
 
@@ -39,19 +41,18 @@ export function ListItem({ item, listPath }) {
 
 				await updateItem(listPath, id, { ...updatedItem });
 			} catch (error) {
-				alert(`Item was not marked as purchased`, error.message);
+				toast.error(`Item was not marked as purchased`, error.message);
 			}
 		}
 	};
 
 	const handleDeleteItem = async () => {
-		if (confirm(`Are you sure you want to delete this item?`)) {
-			try {
-				await deleteItem(listPath, id);
-			} catch (error) {
-				alert('Item was not deleted');
-			}
+		try {
+			await deleteItem(listPath, id);
+		} catch (error) {
+			toast.error('Item was not deleted');
 		}
+
 		return;
 	};
 
