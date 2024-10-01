71e23dcc5eb3ab19bd1f3b2a2aa7ef53e8974e93
Nika Kolesnikova <firstnamenika@gmail.com>
2024-10-01 16:21:53 -0400

    feat: implement useImportance hook: - display pre-sorted by importance lists; - pass isImportant prop to each SingleList component

--- src/components/SingleList.jsx
+++ src/components/SingleList.jsx
@@ -1,13 +1,41 @@
 import './SingleList.css';
+import { useState } from 'react';
+import PushPinIcon from '@mui/icons-material/PushPin';
 
-export function SingleList({ name, path, setListPath }) {
-	function handleClick() {
+export function SingleList({
+	name,
+	path,
+	setListPath,
+	setImportantList,
+	isImportant,
+}) {
+	const [isHovered, setIsHovered] = useState(false);
+
+	const handleClick = () => {
 		setListPath(path);
-	}
+	};
+
+	const handleImportantList = () => {
+		setImportantList(path);
+	};
+
+	const pinStyle = isImportant ? 'visible' : !isHovered ? 'hidden' : 'visible';
 
 	return (
-		<li className="SingleList">
-			<button onClick={handleClick}>{name}</button>
-		</li>
+		<>
+			<li
+				className={`SingleList ${isHovered && 'hovered'}`}
+				onMouseEnter={() => setIsHovered(true)}
+				onMouseLeave={() => setIsHovered(false)}
+			>
+				<PushPinIcon
+					fontSize="large"
+					className={pinStyle}
+					onClick={handleImportantList}
+				/>
+
+				<button onClick={handleClick}>{name}</button>
+			</li>
+		</>
 	);
 }
