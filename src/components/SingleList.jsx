a5f6a7cd8301354fc2facb02098dcab8da13c648
Nika Kolesnikova <firstnamenika@gmail.com>
2024-10-03 15:37:39 -0400

    feat: introduce logic that removes a list from users view and/or from firebase; add alerts and a material DeleteIcon

--- src/components/SingleList.jsx
+++ src/components/SingleList.jsx
@@ -1,11 +1,18 @@
-import { useNavigate } from 'react-router-dom';
 import './SingleList.css';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import PushPinIcon from '@mui/icons-material/PushPin';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { MaterialListButton, MaterialButton } from './material-buttons';
+import { useAuth, deleteList } from '../api';
+
+const deletionResponse = {
+	hard: `List deleted permanently.`,
+	soft: `List removed from user view.`,
+};
 
 export function SingleList({
-	name,
-	path,
+	item,
 	setListPath,
 	setImportantList,
 	isImportant,
@@ -13,6 +20,12 @@ export function SingleList({
 	const [isHovered, setIsHovered] = useState(false);
 
 	const navigate = useNavigate();
+	const { user } = useAuth();
+	const userId = user?.uid;
+	const userEmail = user?.email;
+
+	const { name, path } = item;
+	const listPath = path.slice(0, path.indexOf('/'));
 
 	const handleClick = () => {
 		setListPath(path);
@@ -25,23 +38,50 @@ export function SingleList({
 		setImportantList(path);
 	};
 
-	const pinStyle = isImportant ? 'visible' : !isHovered ? 'hidden' : 'visible';
+	const handleDelete = async () => {
+		const deletionType = listPath === userId ? 'hard' : 'soft';
+
+		if (confirm(`Are you sure you want to delete ${name}?`)) {
+			try {
+				await deleteList(deletionType, path, userId, userEmail);
+				alert(deletionResponse[deletionType]);
+			} catch (error) {
+				alert(
+					`List was not deleted. Error: ${error.message ? error.message : error}`,
+				);
+			}
+		}
+		return;
+	};
+
+	const pinStyle = {
+		visibility: isImportant || isHovered ? 'visible' : 'hidden',
+	};
+
+	const deleteStyle = {
+		visibility: isHovered ? 'visible' : 'hidden',
+	};
 
 	return (
-		<>
-			<li
-				className={`SingleList ${isHovered && 'hovered'}`}
-				onMouseEnter={() => setIsHovered(true)}
-				onMouseLeave={() => setIsHovered(false)}
-			>
-				<PushPinIcon
-					fontSize="large"
-					className={pinStyle}
-					onClick={handleImportantList}
-				/>
+		<li
+			className={`SingleList ${isHovered && 'hovered'}`}
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+		>
+			<MaterialButton
+				variant="text"
+				style={pinStyle}
+				onClick={handleImportantList}
+				startIcon={<PushPinIcon />}
+			/>
 
-				<button onClick={handleClick}>{name}</button>
-			</li>
-		</>
+			<MaterialListButton onClick={handleClick} buttonText={name} />
+			<MaterialButton
+				variant="text"
+				onClick={handleDelete}
+				style={deleteStyle}
+				startIcon={<DeleteIcon />}
+			/>
+		</li>
 	);
 }
