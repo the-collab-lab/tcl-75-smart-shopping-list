0ff1edad7d0c6ebeeb3d75a60dc9fce1d651b635
Nika Kolesnikova <firstnamenika@gmail.com>
2024-10-14 16:41:32 -0400

    test: update SingleList test

--- tests/SingleList.test.jsx
+++ tests/SingleList.test.jsx
@@ -2,14 +2,23 @@ import { render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import { SingleList } from '../src/components/SingleList';
+import { deleteList } from '../src/api';
 import {
 	mockSingleListItem,
 	mockSetImportantList,
 	mockSetListPath,
 } from '../src/mocks/__fixtures__/singleListData';
+import { mockUser } from '../src/mocks/__fixtures__/auth';
+
+vi.mock('../src/api', () => ({
+	deleteList: vi.fn(),
+	useAuth: () => ({
+		user: { uid: mockUser.uid, email: mockUser.email },
+	}),
+}));
 
 describe('SingleList component', () => {
-	test('hovers delete icon', async () => {
+	test('calls deleteList function', async () => {
 		render(
 			<MemoryRouter>
 				<SingleList
@@ -21,9 +30,21 @@ describe('SingleList component', () => {
 			</MemoryRouter>,
 		);
 
-		const listItem = screen.getByRole('listitem');
-		await userEvent.hover(listItem);
+		const deleteIcon = screen.getByTestId('DeleteOutlineOutlinedIcon');
+		await userEvent.click(deleteIcon);
+		const confirmDialog = await screen.findByText(
+			`Are you sure you want to delete ${mockSingleListItem.name}?`,
+		);
+		expect(confirmDialog).toBeInTheDocument();
 
-		expect(screen.getByTestId('DeleteIcon')).toBeInTheDocument();
+		const confirmButton = screen.getByText('Confirm');
+		await userEvent.click(confirmButton);
+
+		expect(deleteList).toHaveBeenCalledWith(
+			'soft',
+			mockSingleListItem.path,
+			mockUser.uid,
+			mockUser.email,
+		);
 	});
 });
