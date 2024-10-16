76e3090648fc69c0e20a269489edb8d0505617f9
Nika Kolesnikova <firstnamenika@gmail.com>
2024-10-16 16:16:41 -0400

    feat: add MUI components and minor custom styling

--- src/components/RadioInputElement.jsx
+++ src/components/RadioInputElement.jsx
@@ -1,15 +1,33 @@
+import { Radio, Tooltip, FormControlLabel } from '@mui/material';
+import { tooltipStyle } from './DeleteIconWithTooltip';
+
+const radioStyle = {
+	color: 'white',
+	m: 2,
+	my: 3,
+};
+
 export const RadioInputElement = ({ label, id, value, required }) => {
 	return (
-		<>
-			<input
-				type="radio"
-				name="purchase-date"
-				id={id}
-				value={value}
-				required={required}
-			/>
-			<label htmlFor={id}>{label}</label>
-			<br />
-		</>
+		<FormControlLabel
+			control={
+				<Radio
+					value={value}
+					id={id}
+					required={required}
+					inputProps={{ 'aria-label': label }}
+					color="success"
+					sx={radioStyle}
+				/>
+			}
+			label={
+				<Tooltip
+					title={<p style={tooltipStyle}>{`${value} days from today`}</p>}
+					arrow
+				>
+					<span style={{ fontSize: '1.8rem' }}>{label}</span>
+				</Tooltip>
+			}
+		/>
 	);
 };
